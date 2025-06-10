598d87b858de68211b90c84dae3e99296812d0dc
Nhu Ha <hamynhu161@gmail.com>
2025-06-10 12:27:07 +0300

    Fix: ensure DRF calls model's save() for Lainaus to make sure book status is updated

--- app/serializers.py
+++ app/serializers.py
@@ -15,3 +15,16 @@ class LainausSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lainaus
         fields = ['id', 'kirja', 'asiakas', 'lainattu_aika', 'palautettu_aika']
+    
+    # luo objekti ja kutsu save() funktiota
+    def create(self, validated_data):
+        lainaus = Lainaus(**validated_data)     #puruta data, jotta Django voi ymmärtää ja luoda Lainaus-objektin ennen mukautetun logiikkasi suorittamista models:ssa
+        lainaus.save()  
+        return lainaus
+
+    # Päivitä jokainen ominaisuus manuaalisesti
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save() 
+        return instance
